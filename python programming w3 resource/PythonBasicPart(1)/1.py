e59b568f89b7fba25ef080a02f334c821be650cb
Muhammad Ismail <m.ismail110022@gmail.com>
2023-12-13 10:57:48 +0500

    Firt commit of Data Science

---
+++ python programming w3 resource/PythonBasicPart(1)/1.py
@@ -0,0 +1,2 @@
+txt ="Twinkle, twinkle, little star,\n \t How I wonder what you are!\n\t\t Up above the world so high,\n\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star,\n \t How I wonder what you are!"
+print(txt)
